125ea72dacaf1be84b356d4a1c24200877eb659f
Murphy Stevens <murphy@Murphys-MacBook-Pro.local>
2019-12-12 21:19:24 -0600

    Added project list component

--- web/simply-projects/src/app/app.module.ts
+++ web/simply-projects/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule,
